0d37d57331bc4647ca77404808d519708a1bb8b2
Parth Verma <v.parth98@gmail.com>
2018-09-22 16:06:36 +0530

    Server setup

--- sonal_maam/settings.py
+++ sonal_maam/settings.py
@@ -78,15 +78,12 @@ WSGI_APPLICATION = 'sonal_maam.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-else:
-    DATABASES = {'default': dj_database_url.config(default=os.environ['DATABASE_URL'])}
+}
 
 
 # Password validation
