81082f04dd84e79a31272be58e3db3989e46c354
Parth Verma <v.parth98@gmail.com>
2018-09-22 20:43:45 +0530

    Set debug as false

--- post-copy-setup.sh
+++ post-copy-setup.sh
@@ -6,6 +6,6 @@ source ./env/bin/activate
 pip install -r requirements.txt
 python manage.py migrate
 bash static_handler.sh
-sudo systemctl rc-local restart
+sudo systemctl restart rc-local.service
 sudo service nginx restart
 exit
