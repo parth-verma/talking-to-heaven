39f9b787b696dc4b284aee8cda4015987a172113
Parth Verma <v.parth98@gmail.com>
2018-09-22 18:52:21 +0530

    Travis config #6

--- pre-copy-setup.sh
+++ pre-copy-setup.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+set -e
 su - parth
 cd /home/parth/sonal_maam
 npm i
+git remote add origin https://github.com/party98/talking-to-heaven.git || \
 git remote set-url origin https://github.com/party98/talking-to-heaven.git
 git pull origin master
 exit
