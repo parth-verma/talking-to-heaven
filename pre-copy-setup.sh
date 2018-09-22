26922fca950378ebb4c13f2a032e5511a3a2b259
Parth Verma <v.parth98@gmail.com>
2018-09-22 19:28:58 +0530

    Travis config #11

--- pre-copy-setup.sh
+++ pre-copy-setup.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
 set -e
+echo "Setting User"
 su - parth
+echo "Changing Directory"
 cd /home/parth/sonal_maam
+echo "Installing Node packages"
 npm i
+echo "Configuring git remotes"
 git remote add origin https://github.com/party98/talking-to-heaven.git || \
 git remote set-url origin https://github.com/party98/talking-to-heaven.git
+echo "Downloading latest git repo"
 git pull origin master
 exit
