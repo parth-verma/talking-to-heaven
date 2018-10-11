444587794cbe39ea23a33f632ec02c57702b6013
Parth Verma <v.parth98@gmail.com>
2018-10-11 23:08:51 +0530

    Finalized cart implementation

--- sonal_maam_site/static/js/cart.js
+++ sonal_maam_site/static/js/cart.js
@@ -1,28 +1,37 @@
 var shoppingCart = {
-  aInternal:[],
-  aListener: function(val) {},
-  set a(val) {
-    this.aInternal = val;
-    this.aListener(val);
-  },
-  get a() {
-    return this.aInternal;
-  },
-  registerListener: function(listener) {
-    this.aListener = listener;
-  }
+    aInternal: [],
+    aListener: function (val) {
+    },
+    set a(val) {
+        this.aInternal = val;
+        this.aListener(val);
+    },
+    get a() {
+        return this.aInternal;
+    },
+    registerListener: function (listener) {
+        this.aListener = listener;
+    }
 };
 
-function saveCart(Cart){
-    console.log(Cart);
-Cookies.set('cart',{cart:Cart});
-updateCartCount();
-updateTotal();
+function saveCart(Cart) {
+    Cookies.set('cart', {cart: Cart});
+    updateCartCount();
+    updateTotal();
 }
-function loadCart(){
-shoppingCart.a = Cookies.getJSON('cart').cart || [];
+
+function loadCart() {
+    var cart = Cookies.getJSON('cart');
+    if (cart) {
+        shoppingCart.a = Cookies.getJSON('cart').cart;
+    }
+    else {
+        shoppingCart.a = [];
+    }
 }
-function addToCart(prodID, prodName, prodCost){
+
+
+function addToCart(prodID, prodName, prodCost) {
     var prodQuant = parseInt($('#count-' + prodID.toString()).text());
     var product = {};
     product.id = prodID;
@@ -31,30 +40,78 @@ function addToCart(prodID, prodName, prodCost){
     product.quantity = prodQuant;
     shoppingCart.a.push(product);
     shoppingCart.a = shoppingCart.a;
-    console.log(product);
 
     return true;
 }
 
-function removeFromCart(prodName){
-    shoppingCart = shoppingCart.a.filter(function (value, index, arr) {
-        return value.name !== prodName
+function removeFromCart(prodID) {
+    shoppingCart.a = shoppingCart.a.filter(function (value, index, arr) {
+        return value.id !== prodID
     });
+    $('#item-' + prodID).remove();
+    updateCartCount();
     return true;
 }
 
-function updateCartCount(){
-    $('.cart-value').text(shoppingCart.aInternal.reduce(function (total, product) {
+function updateCartCount() {
+    var total = shoppingCart.aInternal.reduce(function (total, product) {
         return total + product.quantity;
-    },0));
+    }, 0);
+    if (total>9){
+    $('.cart-value').text('+');}
+    else {
+        $('.cart-value').text(total);}
+
 }
 
-function updateTotal(){
-    for( product in shoppingCart.a){
-        var total = product.quantity * product.price;
-        $(`#total-${product.id}`).text(total);
-    }
+function updateTotal() {
+    shoppingCart.aInternal.forEach(function (product) {
+        var total = product.quantity * product.cost;
+        $(`#total-${product.id}`).text('$' + total.toFixed(1).toString());
+    });
+
 }
+
+function decrementCartItem(prodID) {
+    $(`#count-${prodID}`).text(parseInt($(`#count-${prodID}`).text()) - 1);
+    if (parseInt($(`#count-${prodID}`).text()) < 0) {
+        $(`#count-${id}`).text(1);
+    }
+    for (let i = 0; i < shoppingCart.a.length; i++) {
+        if (shoppingCart.a[i].id === prodID) {
+            shoppingCart.a[i].quantity -= 1;
+            break;
+        }
+    }
+    saveCart(shoppingCart.aInternal);
+    return true;
+}
+
+function incrementCartItem(prodID) {
+    $(`#count-${prodID}`).text(parseInt($(`#count-${prodID}`).text()) + 1);
+    for (let i = 0; i < shoppingCart.a.length; i++) {
+        if (shoppingCart.a[i].id === prodID) {
+            shoppingCart.a[i].quantity += 1;
+            break;
+        }
+    }
+    saveCart(shoppingCart.aInternal);
+    return true;
+}
+
+
+function cartSubmit() {
+    var url = 'cart';
+    var form = $('<form action="' + url + '" method="post">' +
+        '<input type="text" name="cart" value="' + encodeURI(JSON.stringify(shoppingCart.a))
+        + '" />' +
+        '</form>');
+    $('body').append(form);
+    form.submit();
+    return true;
+
+}
+
 shoppingCart.registerListener(saveCart);
 $(document).ready(
-loadCart);
+    loadCart);
--- sonal_maam_site/views.py
+++ sonal_maam_site/views.py
@@ -1,6 +1,13 @@
+import json
+import urllib
+
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+
 from sonal_maam_site.models import Reading
 
 
@@ -8,6 +15,22 @@ def index(request):
     readings = Reading.objects.all()
     return render(request, 'index.html',{'readings':readings})
 
-
+@csrf_exempt
 def cart(request):
-    return render(request, 'cart.html')
+    if request.method == "POST":
+        cart_new = request.POST
+        request.session['cart'] = cart_new
+        print(cart_new)
+        return HttpResponseRedirect(reverse('cart'))
+    try:
+        cart_new = json.loads(urllib.parse.unquote(request.session.get('cart')['cart']))
+    except TypeError:
+        cart_new = []
+    request.session.clear()
+    cart_products = {x['id']:x['quantity'] for x in cart_new}
+    cart_items = Reading.objects.filter(pk__in=[x for x in cart_products.keys()])
+    order_items = []
+    for item in cart_items:
+        order_items.append({'name':item.name,'price':item.price,'quantity':cart_products[item.id],'id':item.id,'image_url':item.image.url})
+
+    return render(request, 'cart.html',{'cart_items':order_items})
